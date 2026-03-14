87ce1629ef4fbbd1e097c6f752d43cf15587f208
achi22 <your-achintyamandal606@gmail.com>
2026-03-15 00:58:54 +0530

    Fixed admin routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import PrintKaro from './App.jsx'
+import PrintKaaro from './App.jsx'
+import AdminDashboard from './Admin.jsx'
 import './index.css'
+
+function Router() {
+  const [isAdmin, setIsAdmin] = React.useState(window.location.hash === "#admin");
+
+  React.useEffect(() => {
+    const handleHash = () => setIsAdmin(window.location.hash === "#admin");
+    window.addEventListener("hashchange", handleHash);
+    return () => window.removeEventListener("hashchange", handleHash);
+  }, []);
+
+  return isAdmin ? <AdminDashboard /> : <PrintKaaro />;
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <PrintKaro />
+    <Router />
   </React.StrictMode>,
 )
